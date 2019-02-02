cf17ce84bb5e8c02568f25100dde2ff9046c1a56
Michael Zeleniuk <zell.xp@gmail.com>
2019-02-02 14:41:38 +0200

    Create a Header component

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+export const Header = () => (
+    <header>
+        <img className="logo" src={logo} alt="Logo" />
+    </header>
+);
