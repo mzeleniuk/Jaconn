89b7bfbe64bb3164729fbda3a1c005a7e43012a4
Michael Zeleniuk <zell.xp@gmail.com>
2019-02-02 15:34:06 +0200

    Add device type detection logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { UAParser } from 'ua-parser-js';
+
 import './styles/header.css';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const deviceType = new UAParser().getResult().device.type;
+
+if (deviceType) {
+    document.body.classList.add(deviceType);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
