f8ab7166865233e6b6104b0f679d2f6157655305
Michael Zeleniuk <zell.xp@gmail.com>
2019-02-24 15:06:21 +0200

    Adjust modal window scroll-bar styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Workload from './components/workload';
 import { hideLoader, setDictionary, showLoader } from './redux/actions';
 import { Storage } from './services/storage';
 
-const appVersion = '1.4.0';
+const appVersion = '1.4.1';
 
 class App extends Component {
     constructor(props) {
