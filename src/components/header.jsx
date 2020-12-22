35def338dd1a88ac6ac9028d64039ab062a1d82e
Michael Zeleniuk <zell.xp@gmail.com>
2020-12-22 18:15:38 +0200

    Add the winter holidays image to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import InfoModal from './infoModal';
 import Language from './language';
 import logo from '../images/logo.svg';
+import trees from '../images/trees.svg';
 
 export const Header = () => (
     <header>
@@ -10,7 +11,19 @@ export const Header = () => (
             <img className="logo" src={logo} alt="Logo" />
         </div>
 
+        {isWinterHolidaysPeriod() && (
+            <img className="winter-holidays" src={trees} alt="Holidays" />
+        )}
+
         <InfoModal />
         <Language />
     </header>
 );
+
+function isWinterHolidaysPeriod() {
+    const currentDate = new Date();
+    const dayNumber = currentDate.getDate();
+    const monthNumber = currentDate.getMonth() + 1;
+
+    return (dayNumber > 20 && monthNumber === 12) || (dayNumber < 14 && monthNumber === 1);
+}
