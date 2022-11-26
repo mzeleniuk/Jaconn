b1fdc03f3dc521a8353aef8bb110fbffb8eb99dd
Michael Zeleniuk <zell.xp@gmail.com>
2022-11-26 18:30:21 +0200

    Disable PWA service worker

--- src/index.js
+++ src/index.js
@@ -22,4 +22,4 @@ root.render(<Provider store={store}><App /></Provider>);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
